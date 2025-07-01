fadeb99b0706af921a05a99139b485d1a349a4b2
재민 <jjm0203311@naver.com>
2025-07-02 01:36:47 +0900

    community dto 최신화, 확정

--- src/main/java/com/example/moneyway/community/dto/request/common/BasePostRequest.java
+++ src/main/java/com/example/moneyway/community/dto/request/common/BasePostRequest.java
@@ -2,12 +2,15 @@ package com.example.moneyway.community.dto.request.common;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
 import java.util.List;
 
 /**
  * 게시글 생성 및 수정 시 공통으로 사용되는 필드를 정의한 추상 클래스입니다.
  * - 제목, 내용, 총 지출, 썸네일 이미지, 첨부 이미지 목록을 포함합니다.
  */
+@Getter
 public abstract class BasePostRequest {
 
     @NotBlank(message = "제목은 필수입니다.")
