83839df410b1c8806ec82f91f08b562ef9f2ce67
재민 <jjm0203311@naver.com>
2025-07-05 18:24:37 +0900

    User Update dto request

--- src/main/java/com/example/moneyway/user/dto/request/UpdateNicknameRequest.java
+++ src/main/java/com/example/moneyway/user/dto/request/UpdateNicknameRequest.java
@@ -1,10 +1,16 @@
 package com.example.moneyway.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor // [개선]
 public class UpdateNicknameRequest {
-    @NotBlank
-    private String newNickname;
+
+    @NotBlank(message = "새 닉네임은 필수입니다.")
+    // 회원가입과 동일한 닉네임 규칙 적용
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+    private final String newNickname;
 }
