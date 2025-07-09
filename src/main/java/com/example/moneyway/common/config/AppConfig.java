00ba8cebe7dfd1de5e829124ad7732d1d5dff164
재민 <jjm0203311@naver.com>
2025-07-09 18:42:57 +0900

    배포설정 수정

--- src/main/java/com/example/moneyway/common/config/AppConfig.java
+++ src/main/java/com/example/moneyway/common/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.example.moneyway.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,8 +16,5 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
+
 }
