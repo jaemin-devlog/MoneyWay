718e7dbb926e4957ad81b70bf39ba9f7198ac8ea
재민 <jjm0203311@naver.com>
2025-07-17 03:49:36 +0900

    기존 Plan 패키지 삭제

--- src/main/java/com/example/moneyway/plan/dto/request/PlanStyleRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.moneyway.plan.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class PlanStyleRequest {
-    private String travelStyle; // 예: "힐링", "액티비티"
-}
--- src/main/java/com/example/moneyway/plan/repository/PlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.moneyway.plan.repository;
-
-import com.example.moneyway.plan.domain.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
